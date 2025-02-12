4a99abe385df17de3821f2f030c5e411f4f307fe
AssumedDialogue <114254777+AssumedDialogue@users.noreply.github.com>
2025-02-12 14:28:22 -0500

    Intial Commit

---
+++ src/main/java/ca/sheridancollege/minhaja/onlinebookstoreareeba/OnlineBookStoreAreebaApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.minhaja.onlinebookstoreareeba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoreAreebaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookStoreAreebaApplication.class, args);
+    }
+
+}
